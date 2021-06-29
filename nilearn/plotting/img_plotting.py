9a22b3f20dc6aa81f1fb48fa85ef6272c20fed58
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-06-29 15:34:36 +0200

    [circle full] increase resolution

--- nilearn/plotting/img_plotting.py
+++ nilearn/plotting/img_plotting.py
@@ -398,7 +398,7 @@ class _MNI152Template(SpatialImage):
 
     def load(self):
         if self.data is None:
-            anat_img = load_mni152_template()
+            anat_img = load_mni152_template(resolution=1)
             anat_img = reorder_img(anat_img)
             data = get_data(anat_img)
             data = data.astype(np.float64)
